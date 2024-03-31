451a6b4df957704ac13800e2cadf86d57f319686
mef <mefud@mail.ru>
2024-03-31 23:15:49 +0500

    31/04/2024

---
+++ turtles.pas
@@ -0,0 +1,15 @@
+program turtles;
+var
+   m: string;
+begin
+   writeln('What is your name?');
+   read(m);
+   if m = 'Nikita' then
+      writeln('Yes, you are Leonardo!');
+   if m = 'Egor' then
+      writeln('Eee boy! You - Donatello!');
+   if m = 'Pavel' then
+      writeln('Fucking shit! You are Raf!');
+   if m = 'Ivan' then
+      writeln('Yahoo!!!! You are Miky! KAVABANGA!!!')
+end.
